bbad08d7a4c9ffc8cace78f76be2c6ad977a2e05
Juanin6 <jn.suarez@uniandes.edu.co>
2025-09-23 12:37:23 -0500

    feat(project): initialize core structure with all the modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventLogModule } from './event-log/event-log.module';
+import { UserModule } from './user/user.module';
+import { HostProfileModule } from './host-profile/host-profile.module';
+import { SpaceModule } from './space/space.module';
+import { InventorySlotModule } from './inventory-slot/inventory-slot.module';
+import { BookingModule } from './booking/booking.module';
+import { AccessCredentialModule } from './access-credential/access-credential.module';
+import { ReviewModule } from './review/review.module';
+import { IncidentModule } from './incident/incident.module';
+
 
 @Module({
-  imports: [],
+  imports: [EventLogModule, UserModule, HostProfileModule, SpaceModule, InventorySlotModule, BookingModule, AccessCredentialModule, ReviewModule, IncidentModule],
   controllers: [AppController],
   providers: [AppService],
 })
