257d36631734b325d19d8326b1d5fb47e679fc1b
Juanin6 <jn.suarez@uniandes.edu.co>
2025-10-01 10:24:47 -0400

    feat(service) : Space service (filter , list all ,sort by price)

--- src/space/space.module.ts
+++ src/space/space.module.ts
@@ -1,4 +1,13 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { SpaceService } from './space.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Space } from './entities/space.entity/space.entity';
+import { SpaceController } from './space.controller';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Space])],
+  providers: [SpaceService],
+  controllers: [SpaceController]
+})
 export class SpaceModule {}
